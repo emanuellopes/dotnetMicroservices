90caaebe8184c173a2b91221b88e114c7d6967f6
Emanuel Lopes <emanuel.lopes.pt@gmail.com>
2022-02-10 22:07:57 +0000

    finished controller

--- src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -48,7 +48,7 @@ namespace Discount.API.Repositories
 
             var affected =
                 await connection.ExecuteAsync(
-                    "UPDATE Coupon SET ProductName=@ProductName, Description=@Description, Amount=@Amount WHERE Id = @Id)",
+                    "UPDATE Coupon SET ProductName=@ProductName, Description=@Description, Amount=@Amount WHERE Id = @Id",
                     new
                     {
                         ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount,
